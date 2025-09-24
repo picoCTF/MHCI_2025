08bf157dbaafe2d6c9019e198a49accffeace586
Melissa-Gibney <120681456+Melissa-Gibney@users.noreply.github.com>
2025-09-24 09:58:31 -0400

    Updated profile page inputs and FocusWrapperCard
    Changed the App.tsx profile page to use the mock data. Updated the FocusWrapperCard to use a transparent background if not in focus.

--- frontend/react_app/src/components/homepage/FocusWrapperCard.tsx
+++ frontend/react_app/src/components/homepage/FocusWrapperCard.tsx
@@ -10,15 +10,17 @@ export interface FocusWrapperCardProps {
 //If this is in focus, then the children will be highlighted by a blue, padded div
 const FocusWrapperCard: React.FC<FocusWrapperCardProps> = ({ children, isFocused, width }) => {
 
-    //Only have padding if the children are in focus
     let padding = "0";
+    let bgColor = "bg-transparent";
 
+    //Only have padding and blue background if the children are in focus
     if(isFocused) {
         padding = "8";
+        bgColor = "bg-primary-100";
     }
 
     return (
-        <Card className={"flex flex-col m-0 bg-primary-100 w-" + (width ? "[" + width + "px]" : "full")+ " h-fit p-" + padding}>
+        <Card className={"flex flex-col m-0 " + bgColor + " w-" + (width ? "[" + width + "px]" : "full")+ " h-fit p-" + padding}>
             <CardBody>
                 {children}
             </CardBody>
