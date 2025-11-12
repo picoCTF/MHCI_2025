b06191c24ff56c2b73775955664a24aa0afa851d
Melissa Gibney <120681456+Melissa-Gibney@users.noreply.github.com>
2025-11-12 16:54:03 -0500

    Moved files from Homepage to homepage and General to general

--- frontend/react_app/src/api_interfaces/learning_path/learningPathModuleItem.tsx
+++ frontend/react_app/src/api_interfaces/learning_path/learningPathModuleItem.tsx
@@ -1,8 +1,9 @@
+import type { Game } from "../2023_generated_interfaces/game/game";
 import type { Challenge } from "../challenge";
 import type { ResourceGroup } from "../resource/resourceGroup";
 
 export interface LPModuleItem {
-    content: Challenge | ResourceGroup; // The item content
+    content: Challenge | ResourceGroup | Game; // The item content
     id: number;
     status: "Complete" | "Incomplete";
 }
